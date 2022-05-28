24f278ddbeb771d859c2ce02b12f5b453b4e9dbd
lz-junior <correajr80@hotmail.com>
2022-05-28 18:39:55 -0300

    Upload the project

---
+++ modelcopia001/script.js
@@ -0,0 +1,45 @@
+function somar() {
+    var somn1 = window.document.getElementById('somnum1')
+    var somn2 = window.document.getElementById('somnum2')
+    var somres = window.document.getElementById('somresultado')
+    var son1 = Number(somn1.value)
+    var son2 = Number(somn2.value)
+    var so = son1 + son2
+    somres.innerHTML = `A soma entre ${son1} e ${son2} é de <strong>${so}</strong>`
+}
+
+function subtrair() {
+    var subn1 = window.document.getElementById('subnum1')
+    var subn2 = window.document.getElementById('subnum2')
+    var subres = window.document.getElementById('subresultado')
+    var sun1 = Number(subn1.value)
+    var sun2 = Number(subn2.value)
+    var su = sun1 - sun2
+    subres.innerHTML = `A subtração entre ${sun1} e ${sun2} é de <strong>${su}</strong>`
+}
+
+function multiplicar() {
+    var muln1 = window.document.getElementById('mulnum1')
+    var muln2 = window.document.getElementById('mulnum2')
+    var mulres = window.document.getElementById('mulresultado')
+    var mun1 = Number(muln1.value)
+    var mun2 = Number(muln2.value)
+    var mu = mun1 * mun2
+    mulres.innerHTML = `A multiplicação entre ${mun1} e ${mun2} é de <strong>${mu}</strong>`
+}
+
+function dividir() {
+    var divn1 = window.document.getElementById('divnum1')
+    var divn2 = window.document.getElementById('divnum2')
+    var divres = window.document.getElementById('divresultado')
+    var din1 = Number(divn1.value)
+    var din2 = Number(divn2.value)
+    var di = din1 / din2
+    divres.innerHTML = `A divisão entre ${din1} e ${din2} é de <strong>${di}</strong>`
+}
+
+
+
+
+
+
